9707dd42e195f94a0a8afbf48dab42e8e0be6362
Hunter Quebedeaux <hunter.quebedeaux@ucf.edu>
2023-12-12 22:56:18 -0500

    change timer to perf_counter

--- quebUtils/tictoc.py
+++ quebUtils/tictoc.py
@@ -2,10 +2,10 @@ import time
 
 class timer():
     def __init__(self):
-        self.ticTime = time.time()
+        self.ticTime = time.perf_counter()
     def tic(self):
-        self.ticTime = time.time()
+        self.ticTime = time.perf_counter()
     def toc(self): 
-        print("\n\tElapsed time is {:.4f} seconds.".format(time.time() - self.ticTime))
+        print("\n\tElapsed time is {:.4f} seconds.".format(time.perf_counter() - self.ticTime))
     def tocVal(self):
-        return time.time() - self.ticTime
+        return time.perf_counter() - self.ticTime
