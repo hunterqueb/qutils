9fb652cbdf93610a9a34e70c7427b640962ec3ad
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2024-04-23 15:22:31 -0400

    updated plotCR3BP function to support multiple view plans and all lagrange points

--- qutils/plot.py
+++ qutils/plot.py
@@ -11,39 +11,57 @@ def plotOrbitPhasePredictions(yTruth,legend=None):
     plt.legend()
     plt.grid()
 
-def plotCR3BPPhasePredictions(yTruth,yTest,epoch=None,t=None,L = 4,earth=True,moon=True):
+def plotCR3BPPhasePredictions(yTruth,yTest,epoch=None,t=None,L = 4,earth=True,moon=True,plane = 'xy'):
     if L == False or L == None:
         L = 0
+    if L == 1:
+        L = [0.8369154703225321,0,0]
+    if L == 2:
+        L = [1.1556818961296604,0,0]
+    if L == 3:
+        L = [-1.0050626166357435,0,0]
     if L == 4:
-        L = [0.48784941,0.86602540]
+        L = [0.48784941,0.86602540,0]
     if L == 5:
-        L = [0.48784941,-0.86602540]
-    m_1 = 5.974E24  # kg
-    m_2 = 7.348E22 # kg
-    mu = m_2/(m_1 + m_2)
+        L = [0.48784941,-0.86602540,0]
+
+    if plane == 'xy':
+        x_idx, y_idx = 0, 1
+        title = 'XY Plane'
+    elif plane == 'xz':
+        x_idx, y_idx = 0, 2
+        title = 'XZ Plane'
+    elif plane == 'yz':
+        x_idx, y_idx = 1, 2
+        title = 'YZ Plane'
+    else:
+        raise ValueError("Invalid plane selection. Choose 'xy', 'xz', or 'yz'.")
+
+    m_1 = 5.974E24  # Mass of Earth in kg
+    m_2 = 7.348E22  # Mass of Moon in kg
+    mu = m_2 / (m_1 + m_2)
+
     plt.figure()
-    plt.plot(yTruth[:,0], yTruth[:,1], label='Truth')
-    plt.plot(yTest[:,0], yTest[:,1], label='NN')
+    plt.plot(yTruth[:, x_idx], yTruth[:, y_idx], label='Truth')
+    plt.plot(yTest[:, x_idx], yTest[:, y_idx], label='NN')
+
     if earth:
-        plt.plot(-mu,0,'ko',label='Earth')
+        plt.plot(-mu if x_idx == 0 else 0, 0 if y_idx in [1, 2] else -mu, 'ko', label='Earth')
     if moon:
-        plt.plot(1-mu,0,'go',label='Moon')
-    if L != 0:
-        plt.plot(L[0],L[1],'d',color='grey',label='Lagrange Point')
-    plt.title('Planar CR3BP')
-    plt.xlabel('x')
-    plt.ylabel('y')
+        plt.plot((1 - mu) if x_idx == 0 else 0, 0 if y_idx in [1, 2] else (1 - mu), 'go', label='Moon')
+    plt.plot(L[x_idx], L[y_idx], 'd', color='grey', label='Lagrange Point')
+
+    plt.title(f'Planar CR3BP: {title}')
+    plt.xlabel(['x', 'z'][x_idx // 2])
+    plt.ylabel(['y', 'z'][y_idx % 2])
     plt.axis('square')
-
     plt.subplots_adjust(left=0.15, right=0.95, bottom=0.15, top=0.95)
-
     plt.tight_layout()
     plt.legend()
-    plt.grid()
-    if epoch == None:
-        return
-    else:
-        plt.savefig('predict/predict%d.png' % epoch)
+    plt.grid(True)
+
+    if epoch is not None:
+        plt.savefig(f'predict/predict{epoch}.png')
         plt.close()
 
 def plotOrbitPredictions(yTruth,yTest,epoch=None,t=None):
