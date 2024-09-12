4ee53f7f8478401ba0b024012b5fca1ab7ce4a0b
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2024-09-12 15:50:43 -0400

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
       name="qutils",
-      version="0.9",
+      version="0.10",
       description="General utilities for engineering research in Python.",
       author='Hunter Quebedeaux',
       packages=find_packages(),
