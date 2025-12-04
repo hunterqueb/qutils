4aca66c646daea910c1963f84adf48282013e071
Hunter Quebedeaux <hunter.quebedeaux@ucf.edu>
2025-12-04 12:40:01 -0500

    removed shap analysis for hardware testing

--- qutils/ml/__init__.py
+++ qutils/ml/__init__.py
@@ -8,4 +8,6 @@ from .regression import *
 from .superweight import *
 from .trialSolution import *
 from .utils import *
-from .shap import *
+# check if on python 3.6, if so, remove .shap import - shap analysis cannot run on hardware testing environment
+if __import__('sys').version_info >= (3, 7):
+    from .shap import *
